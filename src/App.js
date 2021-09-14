514ffd9ac4494569cee34b9c92497d740e36b522
Josh Showalter <joshua.showalter@accenture.com>
2021-09-14 17:36:05 -0600

    Lazy loading; only scroll main section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
+import { Suspense, lazy } from 'react';
 import Sidebar from './components/Sidebar';
-import Home from './components/Home';
-import About from './components/About';
-import Reddit from './components/Reddit';
 import Error from './components/Error';
 import { Switch, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./components/Home'));
+const About = lazy(() => import('./components/About'));
+const Reddit = lazy(() => import('./components/Reddit'));
 
 function App() {
   return (
     <div className="h-screen flex">
       <Sidebar /> 
-      <main style={{width: `calc(100% - 220px)`}} className="h-full bg-gray-50">
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-          <Route path="/reddit" component={Reddit} />
-          <Route component={Error} />
-        </Switch>
+      <main style={{width: `calc(100% - 220px)`}} className="h-full bg-gray-50 overflow-y-auto">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/reddit" component={Reddit} />
+            <Route component={Error} />
+          </Switch>
+        </Suspense>
       </main>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,20 @@ import Card from './Card';
 
 const cards = [
   {
-    title: 'Card 1'
+    title: '1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    title: 'Card 2'
+    title: '2',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
   {
-    title: 'Card 3'
+    title: '3',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: '4',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   }
 ];
 
@@ -16,14 +23,18 @@ const cardList = cards.map((item, index) => {
   return (
     <Card key={index}>
       <p className="p-4">{item.title}</p>
+      <p className="p-4">{item.content}</p>
     </Card>
   );
 });
 
 export default function Home() {
   return (
-    <div>
-      {cardList}
+    <div className="p-4">
+      <h1 className="p-4 text-2xl">Portfolio</h1>
+      <div className="overflow-scroll">
+        {cardList}
+      </div>
     </div>
   );
 }
