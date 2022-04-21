05cb687bfa46ebf4beff28a8afc8ec0e10a41783
Josh Showalter <joshhshowalter@gmail.com>
2022-04-20 22:24:14 -0600

    initial nextjs commit

--- src/components/Error.js
+++
@@ -1,5 +0,0 @@
-export default function Error() {
-  return (
-    <h1 className="p-4">Error invalid path</h1>
-  );
-}
--- src/components/Reddit.js
+++
@@ -1,7 +0,0 @@
-const siteUrl = "https://redditreact.netlify.app";
-
-export default function Reddit() {
-  return (
-    <iframe className="w-full h-full" src={siteUrl} title="Reddit"></iframe>
-  );
-}
