9bf543f3890e9bf8aacda0733611abae0c0f33c3
Josh Showalter <joshhshowalter@gmail.com>
2023-06-03 16:48:02 -0600

    lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import remarkParse from 'remark-parse';
-import { unified } from 'unified';
-import remarkHtml from 'remark-html';
-import { gql } from '@apollo/client';
+import remarkParse from 'remark-parse'
+import { unified } from 'unified'
+import remarkHtml from 'remark-html'
+import { gql } from '@apollo/client'
 import type { NextPage } from 'next'
-import Head from 'next/head';
+import Head from 'next/head'
 
-import Hero from '../components/hero';
-import About from '../components/about';
-import Experience from '../components/experience';
-import Projects from '../components/projects';
-import Contact from '../components/contact';
-import client from '../apollo-client';
+import Hero from '../components/hero'
+import About from '../components/about'
+import Experience from '../components/experience'
+import Projects from '../components/projects'
+import Contact from '../components/contact'
+import client from '../apollo-client'
 
 export async function getStaticProps() {
   const { data } = await client.query({
@@ -23,29 +23,31 @@ export async function getStaticProps() {
           company
           description
         }
-      } 
-    `
-  });
+      }
+    `,
+  })
 
-  const copy = JSON.parse(JSON.stringify(data.experiences));
-  const result = await Promise.all(copy.map(async (item: any) => {
-    const processedContent = await unified()
-      .use(remarkParse)
-      .use(remarkHtml)
-      .process(item.description);
+  const copy = JSON.parse(JSON.stringify(data.experiences))
+  const result = await Promise.all(
+    copy.map(async (item: any) => {
+      const processedContent = await unified()
+        .use(remarkParse)
+        .use(remarkHtml)
+        .process(item.description)
 
-    item['description'] = processedContent.value
-    return item;
-  }));
+      item['description'] = processedContent.value
+      return item
+    })
+  )
 
   return {
     props: {
-      experiences: result
-    }
-  };
+      experiences: result,
+    },
+  }
 }
 
-const Home: NextPage = ({experiences}: any) => {
+const Home: NextPage = ({ experiences }: any) => {
   return (
     <>
       <Head>
@@ -58,11 +60,13 @@ const Home: NextPage = ({experiences}: any) => {
         <Projects />
         <Contact />
       </main>
-      <footer className="bg-base-300 text-center py-6 px-6 text-sm font-light">
-        <span>Built with Next.js | Design inspiration from Brittany Chiang</span>
+      <footer className="bg-base-300 py-6 px-6 text-center text-sm font-light">
+        <span>
+          Built with Next.js | Design inspiration from Brittany Chiang
+        </span>
       </footer>
     </>
-  );
-} 
+  )
+}
 
 export default Home
